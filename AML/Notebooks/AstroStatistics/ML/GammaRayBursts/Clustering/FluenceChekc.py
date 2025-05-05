b58385381a8bda2eee8399a8105e79f9c0972ac4
RichardforPhys <r.maifredi@campus.unimib.it>
2025-05-05 12:23:37 +0200

    All programs in GRBs have to be reviewed! Insert scaling and division btw train and test when clustering

--- AML/Notebooks/AstroStatistics/ML/GammaRayBursts/Clustering/FluenceChekc.py
+++ AML/Notebooks/AstroStatistics/ML/GammaRayBursts/Clustering/FluenceChekc.py
@@ -2,6 +2,8 @@ import numpy as np
 import sys
 sys.path.append(r'C:\Users\ricca\Documents\Unimib-Code\AstroStatistics\AML\Notebooks\AstroStatistics\ML\GammaRayBursts\Utilities')
 import utils
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
 
 url = 'https://user-web.icecube.wisc.edu/~grbweb_public/Summary_table.txt'
 raw, names = utils.read_data(url)
@@ -18,16 +20,19 @@ T90, fluence = zip(*[(float(a), float(c)) for a, c in zip(raw[6], raw[9])
                                            ])
 
 DD = np.vstack([np.log10(np.array(fluence)), np.log10(np.array(T90))])
+scaler = StandardScaler()
 
 for factor in np.arange(10, 1, -2, dtype=int):
     
     print('Processing factor number...', factor)
     
-    data = DD[:, ::factor].T
-    print(data[0].shape)
+    DD_scaled = scaler.fit_transform(DD)
+    data = DD_scaled[:, ::factor].T
+    X_train , X_test = train_test_split(data, test_size=0.1, random_state=42)
     
-    n_clusters, score = utils.Gauss_peaks(data, 2, 10, False)
-    labels, model = utils.GM_fit(data, n_clusters, False)
+    n_clusters, score = utils.Gauss_peaks(X_train, 2, 10, False)
+    _ , model = utils.GM_fit(X_train, n_clusters, False)
+    labels = model.predict(X_test)
 
     results['factor'].append(factor)
     results['n_clusters'].append(n_clusters)
