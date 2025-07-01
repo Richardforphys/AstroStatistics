524d7f96a5ebac76daff9b184bccaedc3ed5c412
Richardforphys <r.maifredi@campus.unimib.it>
2025-07-01 22:35:57 +0200

    LIGO is done!!!

--- Notebooks/Exam/LIGO/read.py
+++ Notebooks/Exam/LIGO/read.py
@@ -7,9 +7,9 @@ from LIGO_Utils.LigoUtils import load_data, downsample_balanced, downsample_unba
 data_path = r"C:\Users\ricca\OneDrive\Documents\LIGO.h5"
 
 y, data, keys = load_data(data_path)
-y, data = downsample_unbalanced(data, y, 20000, 42)
+y, data = downsample_unbalanced(data, y, 100000, 42)
 
 print('Saving DS data...')
-np.save('y_ds.npy', y)
-np.save('data_ds.npy', data)
-np.save('keys.npy', keys)
+np.save(r'C:\Users\ricca\Documents\Unimib-Code\AstroStatistics\Notebooks\Exam\LIGO\Npy\y_ds_100k.npy', y)
+np.save(r'C:\Users\ricca\Documents\Unimib-Code\AstroStatistics\Notebooks\Exam\LIGO\Npy\data_ds_100k.npy', data)
+np.save(r'C:\Users\ricca\Documents\Unimib-Code\AstroStatistics\Notebooks\Exam\LIGO\Npykeys.npy', keys)
