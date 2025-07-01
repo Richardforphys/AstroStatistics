49983767841f73b2538e3b0836f743d1e2e983b7
Richardforphys <r.maifredi@campus.unimib.it>
2025-07-01 09:01:54 +0200

    Working on LIGO_discriminative_unbalanced

--- Notebooks/Exam/LIGO/read.py
+++ Notebooks/Exam/LIGO/read.py
@@ -7,13 +7,9 @@ from LIGO_Utils.LigoUtils import load_data, downsample_balanced, downsample_unba
 data_path = r"C:\Users\ricca\OneDrive\Documents\LIGO.h5"
 
 y, data, keys = load_data(data_path)
-
-print('Saving data...')
-np.save('data_100k', data)
-np.save('labels_100k', y)
-np.save('keys_100k', keys)
-
 y, data = downsample_unbalanced(data, y, 20000, 42)
+
 print('Saving DS data...')
-np.save('y_ds_100k.npy', y)
-np.save('data_ds_100k.npy', data)
+np.save('y_ds.npy', y)
+np.save('data_ds.npy', data)
+np.save('keys.npy', keys)
